4776059254461a47f16d86e52b5d6d93ab83d1b9
mrayhanhakim <mrayhanhakim2@gmail.com>
2019-02-27 07:43:04 +0700

    add nav-toggle, fix mobile view

--- js/main.js
+++ js/main.js
@@ -2,16 +2,10 @@ document.body.onload = function () {
   /* - START - onclick nav-link change content */
   let page = document.getElementsByClassName('content')
   let link = document.getElementsByClassName('nav-link')
-  // let footer = document.getElementById('footer')
 
   for (let i = 0; i < link.length; i++) {
     link[i].addEventListener('click', function () {
       page[i].classList.add('active')
-      // if (page[i].id !== 'Home') {
-      //   footer.removeAttribute('class')
-      // } else {
-      //   footer.classList.add('sticky')
-      // }
       for (let j = 0; j < page.length; j++) {
         if (page[j].classList.contains('active') && page[i] !== page[j]) {
           page[j].classList.remove('active')
@@ -19,5 +13,9 @@ document.body.onload = function () {
       }
     })
   }
+  /* - END - */
+
+  /* - START - Nav Toggle */
+  let navToggle = document.getElementById('nav-toggle')
   /* - END - */
 }
