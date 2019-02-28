e0be6b5d5b55b999653eda74347dd85ea664c65d
mrayhanhakim <mrayhanhakim2@gmail.com>
2019-02-28 09:31:01 +0700

    add bg.jpg, add nav-toggle function, fix mobile view, add bg-color

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+My Portfolio to join one of the labs in my Univeristy and also for my practice in using HTML/CSS/JavaScript.
--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 document.body.onload = function () {
-  /* - START - onclick nav-link change content */
+  /* Initialize Variables */
   let page = document.getElementsByClassName('content')
   let link = document.getElementsByClassName('nav-link')
+  let navToggle = document.getElementById('nav-toggle')
+  let navDrop = document.getElementById('right-nav-link')
 
+  /* - START - onclick nav-link change content */
   for (let i = 0; i < link.length; i++) {
     link[i].addEventListener('click', function () {
       page[i].classList.add('active')
+      link[i].classList.add('active-nav')
       for (let j = 0; j < page.length; j++) {
-        if (page[j].classList.contains('active') && page[i] !== page[j]) {
+        if (page[j].classList.contains('active') && link[j].classList.contains('active-nav') && page[i] !== page[j] && link[i] !== link[j]) {
           page[j].classList.remove('active')
+          link[j].classList.remove('active-nav')
         }
       }
     })
@@ -16,6 +21,19 @@ document.body.onload = function () {
   /* - END - */
 
   /* - START - Nav Toggle */
-  let navToggle = document.getElementById('nav-toggle')
+  navToggle.addEventListener('click', function () {
+    navDrop.classList.toggle('drop')
+  })
+
+  window.addEventListener('resize', function () {
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+      navDrop.removeAttribute('class')
+      for (let j = 1; j < link.length; j++) {
+        link[j].addEventListener('click', function () {
+          navDrop.classList.toggle('drop')
+        })
+      }
+    }
+  })
   /* - END - */
 }
